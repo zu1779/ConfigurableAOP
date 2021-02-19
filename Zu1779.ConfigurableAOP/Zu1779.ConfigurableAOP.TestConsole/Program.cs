8fafa32afa0293f79c072250755ba4acfcd59c9b
Zu1779 <maurodallazuanna@hotmail.com>
2021-02-19 20:19:17 +0100

    Implementing HybridInterceptor and testing method selector (to be continued with dynamic configurable interceptions).

---
+++ Zu1779.ConfigurableAOP/Zu1779.ConfigurableAOP.TestConsole/Program.cs
@@ -0,0 +1,169 @@
+﻿namespace Zu1779.ConfigurableAOP.TestConsole
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    using LightInject;
+    using LightInject.Interception;
+
+    class Program
+    {
+        static async Task Main()
+        {
+            ServiceContainer serviceContainer = new ServiceContainer();
+            serviceContainer.RegisterFrom<CompositionRoot>();
+
+            Program prg = serviceContainer.GetInstance<Program>();
+            await prg.ProgramTask();
+
+            Console.WriteLine("\r\nPress any key");
+            Console.ReadKey();
+        }
+
+        public Program(IOuterService outerService)
+        {
+            this.outerService = outerService;
+        }
+        private readonly IOuterService outerService;
+
+        public async Task ProgramTask()
+        {
+            await outerService.Task();
+        }
+    }
+
+    class CompositionRoot : ICompositionRoot
+    {
+        public void Compose(IServiceRegistry serviceRegistry)
+        {
+            serviceRegistry.Register<Program>();
+            serviceRegistry.Register<IOuterService, OuterService>();
+            serviceRegistry.Register<IInnerService, InnerService>();
+            serviceRegistry.Register<DebugInterceptor>();
+            serviceRegistry.Register<DataTraceInterceptor>();
+            serviceRegistry.Register<PerformanceInterceptor>();
+            //serviceRegistry.Intercept(sr => sr.ServiceType == typeof(IInnerService), sf => sf.GetInstance<DebugInterceptor>());
+            //serviceRegistry.Intercept(sr => sr.ServiceType == typeof(IInnerService), sf => sf.GetInstance<DataTraceInterceptor>());
+            //serviceRegistry.Intercept(sr => sr.ServiceType == typeof(IInnerService), sf => sf.GetInstance<PerformanceInterceptor>());
+
+            // Method interception
+            Action<IServiceFactory, ProxyDefinition> defineProxy = (sf, pd) =>
+            {
+                pd.Implement(() => sf.GetInstance<DataTraceInterceptor>(), m => m.Name == "Design");
+            };
+            serviceRegistry.Intercept(sr => sr.ServiceType == typeof(IInnerService), (sf, pd) => defineProxy(sf, pd));
+        }
+    }
+
+    class DebugInterceptor : HybridInterceptor
+    {
+        protected override object BeforeInvoke(IInvocationInfo invocationInfo, Dictionary<string, object> additionalData)
+        {
+            Console.WriteLine($"* Before {invocationInfo.TargetMethod.Name}");
+            var response = base.BeforeInvoke(invocationInfo, additionalData);
+            return response;
+        }
+        protected override object AfterInvoke(IInvocationInfo invocationInfo, object response, Dictionary<string, object> additionalData)
+        {
+            Console.WriteLine($"* After {invocationInfo.TargetMethod.Name}");
+            return response;
+        }
+    }
+
+    class DataTraceInterceptor : HybridInterceptor
+    {
+        protected override object AfterInvoke(IInvocationInfo invocationInfo, object response, Dictionary<string, object> _)
+        {
+            Console.WriteLine($"CALL => {invocationInfo.Signature()} => RETURN => {response}");
+            return response;
+        }
+    }
+    class PerformanceInterceptor : HybridInterceptor
+    {
+        protected override object BeforeInvoke(IInvocationInfo invocationInfo, Dictionary<string, object> additionalData)
+        {
+            additionalData.Add("stopwatch", Stopwatch.StartNew());
+            return base.BeforeInvoke(invocationInfo, additionalData);
+        }
+        protected override object AfterInvoke(IInvocationInfo invocationInfo, object response, Dictionary<string, object> additionalData)
+        {
+            Stopwatch sw = additionalData["stopwatch"] as Stopwatch;
+            Console.WriteLine($"CALL => {invocationInfo.Signature()} => TOOK => {sw.ElapsedMilliseconds} ms");
+            return base.AfterInvoke(invocationInfo, response, additionalData);
+        }
+    }
+
+    static class InvocationExtension
+    {
+        public static string Signature(this IInvocationInfo invocationInfo)
+        {
+            string returnType = invocationInfo.Method.ReturnType.ToString();
+            string methodName = invocationInfo.Method.Name;
+            string arguments = string.Join(", ", invocationInfo.Method.GetParameters().Select((c, i) => $"{c.ParameterType} {c.Name} = {invocationInfo.Arguments[i]}"));
+            return $"{returnType} {methodName}({arguments})";
+        }
+    }
+
+    public interface IOuterService
+    {
+        Task Task();
+    }
+    class OuterService : IOuterService
+    {
+        public OuterService(IInnerService innerService)
+        {
+            this.innerService = innerService;
+        }
+        private readonly IInnerService innerService;
+
+        public async Task Task()
+        {
+            Console.WriteLine("Going to make some tasks");
+            innerService.Analyse();
+            innerService.Design();
+            innerService.Develope();
+            await innerService.Log();
+            await innerService.GetTime();
+            //await innerService.Await2sec();
+        }
+    }
+
+    public interface IInnerService
+    {
+        void Analyse();
+        void Design(int layers = 3);
+        void Develope();
+        Task Log();
+        Task<DateTime> GetTime();
+        Task Await2sec();
+    }
+    class InnerService : IInnerService
+    {
+        public void Analyse() => Console.WriteLine("I'm going to analyse");
+        public void Design(int layers = 3) => Console.WriteLine($"I'm going to design {layers} layers");
+        public void Develope() => Console.WriteLine("I'm going to develope");
+
+        public async Task Log()
+        {
+            await Task.Delay(1);
+            Console.WriteLine($"Time to log something");
+        }
+
+        public async Task Await2sec()
+        {
+            await Task.Delay(4000);
+        }
+
+        public async Task<DateTime> GetTime()
+        {
+            var now = await Task.Run(() => DateTime.Now);
+            Console.WriteLine($"Well ... think it's better to check the clock");
+            return now;
+        }
+    }
+}
---
+++ Zu1779.ConfigurableAOP/Zu1779.ConfigurableAOP/HybridInterceptor.cs
@@ -0,0 +1,90 @@
+﻿namespace Zu1779.ConfigurableAOP
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Concurrent;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    using LightInject.Interception;
+
+    public abstract class HybridInterceptor : IInterceptor
+    {
+        static HybridInterceptor()
+        {
+            OpenGenericInvokeAsyncMethod = typeof(HybridInterceptor).GetTypeInfo().DeclaredMethods
+                .FirstOrDefault(m => m.IsGenericMethod && m.Name == "InvokeAsync");
+        }
+        private static readonly MethodInfo OpenGenericInvokeAsyncMethod;
+        private static readonly ConcurrentDictionary<Type, TaskType> TaskTypes = new ConcurrentDictionary<Type, TaskType>();
+        private static readonly ConcurrentDictionary<Type, Func<object, object[], object>> InvokeAsyncDelegates =
+            new ConcurrentDictionary<Type, Func<object, object[], object>>();
+        private readonly IMethodBuilder methodBuilder = new DynamicMethodBuilder();
+
+        public object Invoke(IInvocationInfo invocationInfo)
+        {
+            Type returnType = invocationInfo.Method.ReturnType;
+            TaskType taskType = GetTaskType(returnType);
+
+            if (taskType == TaskType.Task) return InvokeAsync(invocationInfo);
+            else if (taskType == TaskType.TaskOfT) return GetInvokeAsyncDelegate(returnType)(this, new object[] { invocationInfo });
+            else return InnerInvoke(invocationInfo);
+        }
+
+        /// <summary>
+        /// Remember to return base method to return response of intercepted method or just return a value to avoid call.
+        /// </summary>
+        protected virtual object BeforeInvoke(IInvocationInfo invocationInfo, Dictionary<string, object> additionalData) => invocationInfo.Proceed();
+        /// <summary>
+        /// Base implementation just return response parameter (don't need to call that if you override)
+        /// </summary>
+        protected virtual object AfterInvoke(IInvocationInfo invocationInfo, object response, Dictionary<string, object> additionalData) => response;
+        private object InnerInvoke(IInvocationInfo invocationInfo)
+        {
+            Dictionary<string, object> additionalData = new Dictionary<string, object>();
+            object response = BeforeInvoke(invocationInfo, additionalData);
+            return AfterInvoke(invocationInfo, response, additionalData);
+        }
+        protected virtual async Task InvokeAsync(IInvocationInfo invocationInfo)
+        {
+            Dictionary<string, object> additionalData = new Dictionary<string, object>();
+            await (Task)BeforeInvoke(invocationInfo, additionalData);
+            AfterInvoke(invocationInfo, null, additionalData);
+        }
+        protected virtual async Task<T> InvokeAsync<T>(IInvocationInfo invocationInfo)
+        {
+            Dictionary<string, object> additionalData = new Dictionary<string, object>();
+            var response = await (Task<T>)BeforeInvoke(invocationInfo, additionalData);
+            return await Task.FromResult((T)AfterInvoke(invocationInfo, response, additionalData));
+        }
+        private Func<object, object[], object> GetInvokeAsyncDelegate(Type returnType)
+        {
+            Func<object, object[], object> response = InvokeAsyncDelegates.GetOrAdd(returnType, CreateInvokeAsyncDelegate);
+            return response;
+        }
+        private Func<object, object[], object> CreateInvokeAsyncDelegate(Type returnType)
+        {
+            var closedGenericInvokeMethod = CreateClosedGenericInvokeMethod(returnType);
+            return methodBuilder.GetDelegate(closedGenericInvokeMethod);
+        }
+        private static MethodInfo CreateClosedGenericInvokeMethod(Type returnType)
+        {
+            MethodInfo response = OpenGenericInvokeAsyncMethod.MakeGenericMethod(returnType.GetTypeInfo().GenericTypeArguments);
+            return response;
+        }
+
+        private static TaskType GetTaskType(Type returnType) => TaskTypes.GetOrAdd(returnType, ResolveTaskType);
+        private static TaskType ResolveTaskType(Type returnType)
+        {
+            if (IsTask(returnType)) return TaskType.Task;
+            else if (IsTaskOfT(returnType)) return TaskType.TaskOfT;
+            else return TaskType.None;
+        }
+        private static bool IsTask(Type returnType) => returnType == typeof(Task);
+        private static bool IsTaskOfT(Type returnType) =>
+            returnType.GetTypeInfo().IsGenericType && returnType.GetTypeInfo().GetGenericTypeDefinition() == typeof(Task<>);
+
+        private enum TaskType { None, Task, TaskOfT }
+    }
+}
